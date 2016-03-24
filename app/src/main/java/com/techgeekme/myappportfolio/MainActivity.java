503d339fe898be8677872b0443783bb43c18f273
shoaib30 <shoaib30@gmail.com>
2016-03-24 14:35:45 +0530

    extracted strings and refactored code

--- app/src/main/java/com/techgeekme/myappportfolio/MainActivity.java
+++ app/src/main/java/com/techgeekme/myappportfolio/MainActivity.java
@@ -1,13 +1,12 @@
 package com.techgeekme.myappportfolio;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,37 +22,37 @@ public class MainActivity extends Activity {
         btnSpotify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Spotify Streamer",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),R.string.strSpotify,Toast.LENGTH_SHORT).show();
             }
         });
         btnScores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Scores App",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),R.string.strScores,Toast.LENGTH_SHORT).show();
             }
         });
         btnLibrary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Library App",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),R.string.strLibrary,Toast.LENGTH_SHORT).show();
             }
         });
         btnBuildItBigger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Build it Better",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),R.string.strBuild,Toast.LENGTH_SHORT).show();
             }
         });
         btnXYZ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"XYZ Reader",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),R.string.strXyz,Toast.LENGTH_SHORT).show();
             }
         });
         btnCapstone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Capstone: My Own App",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),R.string.strCapstone,Toast.LENGTH_SHORT).show();
             }
         });
     }
